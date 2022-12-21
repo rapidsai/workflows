5195ea7839d161967369d8a20fcbfebd27ca30d8
AJ Schmidt <aschmidt@nvidia.com>
2022-12-21 00:14:55 -0500

    Document `update-branch-version.sh`
    This PR documents `update-branch-versions.sh` and adds some debugging output for when there are errors.

--- scripts/update-branch-versions.sh
+++ scripts/update-branch-versions.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Script to update the RAPIDS and ucx-py branch versions
+# throughout our workflow YAML files.
+# Usage (from project root dir):
+#   ./scripts/update-branch-versions.sh 23.02
 set -euo pipefail
 
 NEW_BRANCH_VERSION=$1
@@ -6,13 +10,15 @@ RAPIDS_BRANCH_PATTERN=[0-9]{2}.[0-9]{2}
 UCX_PY_BRANCH_PATTERN=0.[0-9]{2}
 
 if [[ ! "${NEW_BRANCH_VERSION}" =~ ^${RAPIDS_BRANCH_PATTERN}$ ]]; then
-  echo "Error: Ensure argument matches \"^${RAPIDS_BRANCH_PATTERN}$\" RegEx (e.g. \"23.02\")."
+  echo "Input argument: ${NEW_BRANCH_VERSION}"
+  echo "Error: Ensure input argument matches \"^${RAPIDS_BRANCH_PATTERN}$\" RegEx (e.g. \"23.02\")."
   exit 1
 fi
 
 UCX_PY_VERSION=$(curl -s "https://version.gpuci.io/rapids/${NEW_BRANCH_VERSION}")
 
 if [[ ! "${UCX_PY_VERSION}" =~ ^${UCX_PY_BRANCH_PATTERN}$ ]]; then
+  echo "UCX_PY_VERSION: ${UCX_PY_VERSION}"
   echo "Error: UCX_PY_VERSION does not match \"^${UCX_PY_BRANCH_PATTERN}$\" RegEx (e.g. \"0.30\")."
   exit 1
 fi
