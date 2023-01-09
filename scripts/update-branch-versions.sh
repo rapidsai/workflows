a9c818b1f69fcbb73fb749af175e5482e8737682
Sevag H <shanssian@nvidia.com>
2023-01-09 17:25:20 -0500

    Upkeep/remove pip pipeline (#9)
    Now that we're building pip wheels alongside conda artifacts in the same
    CI workflows, we don't need a separate pip wheel pipeline workflow.
    
    This PR deletes the pip workflow.
    
    ~This PR needs to wait for the associated ucx-py GHA PR to be merged
    first: https://github.com/rapidsai/ucx-py/pull/909~
    
    Co-authored-by: AJ Schmidt <aschmidt@nvidia.com>

--- scripts/update-branch-versions.sh
+++ scripts/update-branch-versions.sh
@@ -7,7 +7,6 @@ set -euo pipefail
 
 NEW_BRANCH_VERSION=$1
 RAPIDS_BRANCH_PATTERN=[0-9]{2}.[0-9]{2}
-UCX_PY_BRANCH_PATTERN=0.[0-9]{2}
 
 if [[ ! "${NEW_BRANCH_VERSION}" =~ ^${RAPIDS_BRANCH_PATTERN}$ ]]; then
   echo "Input argument: ${NEW_BRANCH_VERSION}"
@@ -15,21 +14,7 @@ if [[ ! "${NEW_BRANCH_VERSION}" =~ ^${RAPIDS_BRANCH_PATTERN}$ ]]; then
   exit 1
 fi
 
-UCX_PY_VERSION=$(curl -s "https://version.gpuci.io/rapids/${NEW_BRANCH_VERSION}")
-
-if [[ ! "${UCX_PY_VERSION}" =~ ^${UCX_PY_BRANCH_PATTERN}$ ]]; then
-  echo "UCX_PY_VERSION: ${UCX_PY_VERSION}"
-  echo "Error: UCX_PY_VERSION does not match \"^${UCX_PY_BRANCH_PATTERN}$\" RegEx (e.g. \"0.30\")."
-  exit 1
-fi
-
-
 echo "Replacing RAPIDS branch versions"
 sed -ri \
-  "s/branch-${RAPIDS_BRANCH_PATTERN}/branch-${NEW_BRANCH_VERSION}/g" \
-  .github/workflows/{nightly,pip-wheels}-pipeline.yaml
-
-echo "Replacing ucx-py branch versions"
-sed -ri \
-  "s/branch-${UCX_PY_BRANCH_PATTERN}/branch-${UCX_PY_VERSION}/g" \
-  .github/workflows/{nightly,pip-wheels}-pipeline.yaml
+  "s/rapids_version: .*/rapids_version: \"${NEW_BRANCH_VERSION}\"/g" \
+  .github/workflows/nightly-pipeline.yaml
