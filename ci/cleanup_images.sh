7922301cb684b380099cd48a68cfcaf95055c6fd
ayodeawe <jawe@nvidia.com>
2023-10-26 14:40:15 -0500

    handle pagination

--- ci/cleanup_images.sh
+++ ci/cleanup_images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 org="rapidsai"
 repo="staging"
@@ -19,25 +20,36 @@ delete_image() {
 fetch_tags() {
     local org=$1
     local repo=$2
+    local page=$3
 
     curl --fail-with-body -i \
         -H "Accept: application/json" \
         -H "Authorization: JWT $HUB_TOKEN" \
-        "https://hub.docker.com/v2/namespaces/$org/repositories/$repo/tags"
+        "https://hub.docker.com/v2/namespaces/$org/repositories/$repo/tags?page=$page&page_size=100"
 }
 
-tags_json=$(fetch_tags "$org" "$repo")
+tags_json=$(fetch_tags "$org" "$repo" 1)
+next_page=$(echo "$tags_json" | jq -r '.next')
 
-echo "$tags_json" | jq -c '.results[]' | while read -r tag_info; do
-    last_pushed=$(echo "$tag_info" | jq -r '.tag_last_pushed')
-    last_pushed_ts=$(date --date="$last_pushed" +%s)
+while [ ! -z "$next_page" ] && [ "$next_page" != "null" ]; do
+    echo "$tags_json" | jq -c '.results[]' | while read -r tag_info; do
+        last_pushed=$(echo "$tag_info" | jq -r '.tag_last_pushed')
+        last_pushed_ts=$(date --date="$last_pushed" +%s)
 
-    #age in days
-    current_time=$(date +%s)
-    age=$(( (current_time - last_pushed_ts) / 86400 ))
+        # age in days
+        current_time=$(date +%s)
+        age=$(( (current_time - last_pushed_ts) / 86400 ))
 
-    if [ "$age" -gt 30 ]; then
-        tag_name=$(echo "$tag_info" | jq -r '.name')
-        delete_image "$org" "$repo" "$tag_name"
-    fi
+        if [ "$age" -gt 30 ]; then
+            tag_name=$(echo "$tag_info" | jq -r '.name')
+            delete_image "$org" "$repo" "$tag_name"
+        fi
+    done
+
+    # Extract next page number
+    next_page_num=$(echo "$next_page" | grep -o 'page=[0-9]*' | cut -d "=" -f 2)
+    
+    # Load next page
+    tags_json=$(fetch_tags "$org" "$repo" "$next_page_num")
+    next_page=$(echo "$tags_json" | jq -r '.next')
 done
